ec10fdad7f54c283ed920e85a51ff92479fd1011
Ernest Stevens <einre2005@gmail.com>
2025-02-09 00:59:32 -0500

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
+    //Define the latitude and longitude variables
+    const latitude = 43.92716;
+    const longtitude = -69.992355;
+
     // Call Open-Meteo API
-    const apiUrl = "https://api.open-meteo.com/v1/forecast?latitude=43.9276&longitude=-69.9759&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,rain_sum,showers_sum,snowfall_sum,precipitation_probability_max&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York&models=gfs_seamless"; 
+    const apiUrl = "https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,rain_sum,showers_sum,snowfall_sum,precipitation_probability_max&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York&models=gfs_seamless"; 
 
     fetch(apiUrl)
     .then(response => response.json())
